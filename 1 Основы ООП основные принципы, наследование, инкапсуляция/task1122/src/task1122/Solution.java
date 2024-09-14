52c0b54258ddda3f4097ec6a0b2feccd01d0d7a0
Yury_YurbasNoteBook <yury.connect@gmail.com>
2024-09-14 15:23:33 +0300

    solutions: task1122

--- 1 Основы ООП основные принципы, наследование, инкапсуляция/task1122/src/task1122/Solution.java
+++ 1 Основы ООП основные принципы, наследование, инкапсуляция/task1122/src/task1122/Solution.java
@@ -14,25 +14,30 @@ Requirements:
 6. Класс Bishop должен быть унаследован от класса ChessPieces.
 7. Класс Pawn должен быть унаследован от класса ChessPieces.*/
 
+
 public class Solution {
     public static void main(String[] args) {
     }
 
-    public class King {
+    public class King extends ChessPieces {
     }
 
-    public class Queen {
+    public class Queen extends ChessPieces {
     }
 
-    public class Rook {
+    public class Rook extends ChessPieces {
     }
 
-    public class Knight {
+    public class Knight extends ChessPieces {
     }
 
-    public class Bishop {
+    public class Bishop extends ChessPieces {
     }
 
-    public class Pawn {
+    public class Pawn extends ChessPieces {
+    }
+
+
+    public class ChessPieces {
     }
 }
