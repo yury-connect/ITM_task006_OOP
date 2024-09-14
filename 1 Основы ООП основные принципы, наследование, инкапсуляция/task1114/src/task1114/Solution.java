1cdd3b9ea614aae2c7a3441d840d738135b8a35f
Yury_YurbasNoteBook <yury.connect@gmail.com>
2024-09-14 13:52:35 +0300

    solutions: task1114

--- 1 Основы ООП основные принципы, наследование, инкапсуляция/task1114/src/task1114/Solution.java
+++ 1 Основы ООП основные принципы, наследование, инкапсуляция/task1114/src/task1114/Solution.java
@@ -21,11 +21,11 @@ public class Solution {
 
     }
 
-    public class Lizard {
+    public class Lizard extends Fish {
 
     }
 
-    public class Dinosaur {
+    public class Dinosaur extends Lizard {
 
     }
 
