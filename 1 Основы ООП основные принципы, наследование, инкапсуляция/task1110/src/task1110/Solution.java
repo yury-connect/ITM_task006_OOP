f5f13c8120821eec94f4247dee2bb4cc3782a13f
Yury_YurbasNoteBook <yury.connect@gmail.com>
2024-09-14 13:44:25 +0300

    solutions: task1110

--- 1 Основы ООП основные принципы, наследование, инкапсуляция/task1110/src/task1110/Solution.java
+++ 1 Основы ООП основные принципы, наследование, инкапсуляция/task1110/src/task1110/Solution.java
@@ -13,30 +13,39 @@ Requirements:
 5. В классе Cat должны быть 4 поля.
 6. Все поля класса Cat должны быть private.*/
 
+
 public class Solution {
     public static void main(String[] args) {
     }
 
     public class Cat {
+        private String name;
+        private int age;
+        private int weight;
+        private int speed;
+
 
         public Cat(String name, int age, int weight) {
-
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
         }
 
+
         public String getName() {
-            return null;
+            return name;
         }
 
         public int getAge() {
-            return 0;
+            return age;
         }
 
         public void setWeight(int weight) {
-
+            this.weight = weight;
         }
 
         public void setSpeed(int speed) {
-
+            this.speed = speed;
         }
     }
 }
