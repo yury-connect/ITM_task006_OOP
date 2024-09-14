268f914634f1ec9fde69126c371a1c941b41672d
Yury_YurbasNoteBook <yury.connect@gmail.com>
2024-09-14 14:01:34 +0300

    solutions: task1120

--- 1 Основы ООП основные принципы, наследование, инкапсуляция/task1120/src/task1120/Solution.java
+++ 1 Основы ООП основные принципы, наследование, инкапсуляция/task1120/src/task1120/Solution.java
@@ -2,7 +2,8 @@ package task1120;
 
 /* 
 Пятая правильная «цепочка наследования»
-Расставь правильно «цепочку наследования» в классах: House (дом), Cat (кот), Dog (собака), Car (машина), Animal (животное), Asset (имущество).
+Расставь правильно «цепочку наследования» в классах:
+    House (дом), Cat (кот), Dog (собака), Car (машина), Animal (животное), Asset (имущество).
 
 
 Requirements:
@@ -17,19 +18,19 @@ public class Solution {
     public static void main(String[] args) {
     }
 
-    public class House {
+    public class House extends Asset {
 
     }
 
-    public class Cat {
+    public class Cat extends Animal {
 
     }
 
-    public class Car {
+    public class Car extends Asset {
 
     }
 
-    public class Dog {
+    public class Dog extends Animal {
 
     }
 
